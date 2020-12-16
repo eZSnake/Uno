f694709661dd152988cc54339c0f82f0722d7457
mrehwald <martin.rehwald@gmail.com>
2020-12-15 22:40:05 -0800

    refactoring UnoGame take 1

--- src/UnoGame.java
+++ src/UnoGame.java
@@ -3,7 +3,7 @@ public class UnoGame { //Eike Rehwald
     private Card placePile;
     private final Hand[] hands;
     private boolean rev = false, skip = false;
-
+    
     public UnoGame(int players) {
         deck = new Deck();
         deck.shuffle(players);
@@ -15,135 +15,169 @@ public class UnoGame { //Eike Rehwald
             }
         }
         //Uncomment below to print out all player's starting hands
-//        for (int i = 0; i < hands.length; i++) {
-//            System.out.println("Player " + (i+1) + "'s Hand:");
-//            System.out.println(hands[i].toString());
-//        }
-        placePile = deck.deal();
-    }
-
-    public void playRounds(int player) {
-        while (playable()) {
-            skip = false;
-            System.out.println("\n\n-=-= Player " + (player + 1) + "'s turn =-=-\n");
-            if (!canPlayACard(player)) {
-                System.out.println("You do not have a card to play. A card will be automatically drawn for you");
-                hands[player].addCard(deck.deal());
-                player = nextPlayer(player, hands.length);
-                continue;
-            }
-            System.out.println("Place pile card:\n" + placePile.toString());
-            System.out.println("\nYour cards:\n" + hands[player].toString(placePile));
-            System.out.print("\nWhat card would you like to play or would you like to draw? (1-" + hands[player].length() + "/'draw','d'): ");
-            String cardToPlay = TextIO.getlnString().toLowerCase();
-            boolean actionTaken = false;
-            int cardInt = -1;
-            while (!actionTaken) {
-                if (isInt(cardToPlay)) {
-                    cardInt = Integer.parseInt(cardToPlay);
-                    if (cardInt < 1 || cardInt > hands[player].length()) {
-                        System.out.print("That is an invalid number. Enter one between 1 and " + hands[player].length() + " or 'draw'/'d': ");
-                        cardToPlay = TextIO.getlnString();
-                        cardInt = -1;
-                    } else if (!(hands[player].getCard(cardInt-1)).isPlayable(placePile)) {
-                        System.out.print("That card can't be played. Enter another one or 'draw'/'d': ");
-                        cardToPlay = TextIO.getlnString();
-                        cardInt = -1;
-                    } else {
-                        actionTaken = true;
-                    }
-                } else if (cardToPlay.equals("draw") || cardToPlay.equals("d")) {
+        //        for (int i = 0; i < hands.length; i++) {
+            //            System.out.println("Player " + (i+1) + "'s Hand:");
+            //            System.out.println(hands[i].toString());
+            //        }
+            placePile = deck.deal();
+        }
+        
+        public void playRounds(int player) {
+            while (playable()) {
+                skip = false;
+                System.out.println("\n\n-=-= Player " + (player + 1) + "'s turn =-=-\n");
+                //
+                // check if player has a card they can play
+                //
+                if (!canPlayACard(player)) {
+                    System.out.println("You do not have a card to play. A card will be automatically drawn for you");
+                    // TODO: handle case where there is no card left to draw !
                     hands[player].addCard(deck.deal());
-                    actionTaken = true;
-                } else {
-                    System.out.print("That isn't a valid value. Enter 'draw', 'd', or a number from 1 to " + hands[player].length() + ": ");
-                    cardToPlay = TextIO.getlnString();
+                    player = nextPlayer(player, hands.length);
+                    continue; // restart the round with the next player
                 }
-            }
-            if (cardInt != -1) {
-                placePile = hands[player].getCard(cardInt-1);
-                hands[player].removeCard(cardInt-1);
+                System.out.print("Place pile card:\n" 
+                + placePile.toString()
+                + "\n\nYour cards:\n" 
+                + hands[player].toString(placePile)
+                + "\n\nWhat card would you like to play or would you like to draw? (1-" 
+                + hands[player].length() + "/'draw','d'): ");
+                
+                boolean validPick = false;
+                String cardToPlay = "";
+                
+                while (!validPick) {
+                    cardToPlay = TextIO.getlnString().toLowerCase();
+                    // now let's check if this was a valid pick - otherwise ask to choose again
+                    // criteria to check:
+                    // is numeric or string 'd, draw'
+                    // is 1..handlength
+                    validPick = isValidPick(cardToPlay, hands[player].length());
+                }
+                
+                if (cardToPlay.equals("draw") || cardToPlay.equals("d")) {
+                    hands[player].addCard(deck.deal());
+                    continue;
+                }
+                
+                Integer cardInt = Integer.parseInt(cardToPlay)-1;
+                
+                // play selected card onto placepile
+                placePile = hands[player].getCard(cardInt);
+                
+                // remove selected card from player's hand
+                hands[player].removeCard(cardInt);
                 if (hands[player].length() <= 0) {
                     break;
                 }
+                
+                // prepare next round by evaluating new placecard
                 placePile.specialMove(deck, hands, player, "");
-            }
-            if (placePile instanceof Skip || placePile instanceof Plus2 || placePile instanceof Plus4) {
+                
+                // if switch then reverse direction
+                if (placePile instanceof Switch) {
+                    if (!((Switch) placePile).getHasSwitched()) {
+                        rev = !rev;
+                        ((Switch) placePile).setHasSwitched(true);
+                    }
+                }
+                
+                // handle skip card
                 if (placePile instanceof Skip) {
+                    // make sure the skip card is not open from the previous play
                     if (!((Skip) placePile).getHasSkipped()) {
                         skip = true;
                         ((Skip) placePile).setHasSkipped(true);
                     }
-                } else if (placePile instanceof Plus2) {
+                } 
+                
+                if (placePile instanceof Plus2) {
+                    // make sure the +2 card is not open from the previous play
+                    
                     if (!((Plus2) placePile).getHasSkipped()) {
                         skip = true;
                         ((Plus2) placePile).setHasSkipped(true);
                     }
-                } else {
+                }
+                
+                if (placePile instanceof Plus4) {
+                    // make sure the +4 card is not open from the previous play
+                    
                     if (!((Plus4) placePile).getHasSkipped()) {
                         skip = true;
                         ((Plus4) placePile).setHasSkipped(true);
                     }
                 }
+                
+                
+                player = nextPlayer(player, hands.length);
             }
-            if (placePile instanceof Switch) {
-                if (!((Switch) placePile).getHasSwitched()) {
-                    rev = !rev;
-                    ((Switch) placePile).setHasSwitched(true);
+            System.out.print("\n\n-=-=-=-=-=-=-\n" + determineWinner() + "\n-=-=-=-=-=-=-");
+        }
+        
+        private boolean playable() {
+            for (int i = 0; i < hands.length; i++) {
+                if (hands[i].length() == 0) {
+                    return false;
                 }
             }
-            player = nextPlayer(player, hands.length);
+            return deck.cardsLeft() >= 0;
         }
-        System.out.print("\n\n-=-=-=-=-=-=-\n" + determineWinner() + "\n-=-=-=-=-=-=-");
-    }
-
-    private boolean playable() {
-        for (int i = 0; i < hands.length; i++) {
-            if (hands[i].length() == 0) {
+        
+        private boolean isInt(String card) {
+            try {
+                Integer.parseInt(card);
+            } catch (NumberFormatException nfe) {
                 return false;
             }
+            return true;
         }
-        return deck.cardsLeft() >= 0;
-    }
-
-    private boolean isInt(String card) {
-        try {
-            Integer.parseInt(card);
-        } catch (NumberFormatException nfe) {
+        
+        private int nextPlayer(int currPlayer, int players) {
+            int nextPlayer;
+            if (!rev && !skip) {
+                nextPlayer = Math.floorMod(currPlayer + 1, players);
+            } else if (!rev){
+                nextPlayer = Math.floorMod(currPlayer + 2, players);
+            } else if (!skip) {
+                nextPlayer = Math.floorMod(currPlayer - 1, players);
+            } else {
+                nextPlayer = Math.floorMod(currPlayer-2, players);
+            }
+            return nextPlayer;
+        }
+        
+        private String determineWinner() {
+            for (int i = 0; i < hands.length; i++) {
+                if (hands[i].length() == 0) {
+                    return "Player " + (i+1) + " has won the game!\nCongratulations!";
+                }
+            }
+            return "There are no cards left to draw. It's a tie.";
+        }
+        
+        private boolean canPlayACard(int player) {
+            for (int i = 0; i < hands[player].length(); i++) {
+                if (hands[player].getCard(i).isPlayable(placePile)) {
+                    return true;
+                }
+            }
             return false;
         }
-        return true;
-    }
-
-    private int nextPlayer(int currPlayer, int players) {
-        int nextPlayer;
-        if (!rev && !skip) {
-            nextPlayer = Math.floorMod(currPlayer + 1, players);
-        } else if (!rev){
-            nextPlayer = Math.floorMod(currPlayer + 2, players);
-        } else if (!skip) {
-            nextPlayer = Math.floorMod(currPlayer - 1, players);
-        } else {
-            nextPlayer = Math.floorMod(currPlayer-2, players);
-        }
-        return nextPlayer;
-    }
-
-    private String determineWinner() {
-        for (int i = 0; i < hands.length; i++) {
-            if (hands[i].length() == 0) {
-                return "Player " + (i+1) + " has won the game!\nCongratulations!";
-            }
-        }
-        return "There are no cards left to draw. It's a tie.";
-    }
-
-    private boolean canPlayACard(int player) {
-        for (int i = 0; i < hands[player].length(); i++) {
-            if (hands[player].getCard(i).isPlayable(placePile)) {
+        
+        private boolean isValidPick(String cardToPlay, int cardsOnHand) {
+            if (cardToPlay.equals("draw") || cardToPlay.equals("d")) {
                 return true;
             }
+            if ((!isInt(cardToPlay)) 
+                || (Integer.parseInt(cardToPlay) < 1) 
+                    || (Integer.parseInt(cardToPlay) > cardsOnHand)
+            ) {
+                System.out.print("Invalid pick. Enter a number between 1 and " 
+                + cardsOnHand + " one or 'draw'/'d': ");
+                return false;
+            }
+            return true;
         }
-        return false;
     }
-}
+    
