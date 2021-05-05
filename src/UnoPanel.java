4e27be41b50e6594060aa02cd46f3b3483407d8d
eZSnake <rehwald@gmail.com>
2021-05-04 22:17:55 -0700

    moved menu JPanel setup into seperate method for seperation in anticipation of creating more JPanels

--- src/UnoPanel.java
+++ src/UnoPanel.java
@@ -8,11 +8,20 @@ import java.io.IOException;
 public class UnoPanel extends JPanel {
     public static void main(String[] args) {
         JFrame window = new JFrame("Uno");
-        JPanel container = new JPanel();
-        container.setLayout(new BorderLayout());
 
         UnoPanel panel = new UnoPanel();
         UnoListener listener = new UnoListener(panel);
+
+        window.setContentPane(menu(listener));
+        window.setSize(800,600);
+        window.setLocation(300,300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+    public static JPanel menu(UnoListener listener) {
+        JPanel menu = new JPanel();
+        menu.setLayout(new BorderLayout());
 
         JPanel buttons = new JPanel();
         JButton bot = new JButton("Bot");
@@ -24,11 +33,11 @@ public class UnoPanel extends JPanel {
         JSlider playerCount = new JSlider(2, 4, 2);
         playerCount.addChangeListener(listener);
         playerCount.setMajorTickSpacing(1);
-//        playerCount.setLabelTable(playerCount.createStandardLabels(1));
         playerCount.setPaintTicks(true);
         buttons.add(playerCount);
-        container.add(buttons, BorderLayout.SOUTH);
-        container.add(new JTextArea("Welcome to the game of Uno.\nEach player starts with 7 cards and first with 0 left wins.\nThe same color can go on the same color, " +
+        menu.add(buttons, BorderLayout.SOUTH);
+
+        menu.add(new JTextArea("Welcome to the game of Uno.\nEach player starts with 7 cards and first with 0 left wins.\nThe same color can go on the same color, " +
                 "the same number can go on the same number, and wish cards can go on any card.\nIf you can't go, you draw a card.\n"), BorderLayout.NORTH);
         BufferedImage back = null; //new ImageIcon("UnoCards/back.png").getImage();
         try {
@@ -39,12 +48,13 @@ public class UnoPanel extends JPanel {
         BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
         outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
         JLabel picLabel = new JLabel(new ImageIcon(outputImage));
-        container.add(picLabel, BorderLayout.CENTER);
+        menu.add(picLabel, BorderLayout.CENTER);
+        return menu;
+    }
 
-        window.setContentPane(container);
-        window.setSize(800,600);
-        window.setLocation(300,300);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
+    public static JPanel playingScreen(UnoListener listener) {
+        JPanel playingScreen = new JPanel();
+        playingScreen.setLayout(new BorderLayout());
+        return playingScreen;
     }
 }
